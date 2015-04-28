2e59b560f79ef62c348314bf3dfb5ae268653dde
vinicius.lemos <vinicius.lemos@jahuws0265.frescatto.com>
2015-04-28 19:42:29 -0300

    finalização de alguns tasks

--- src/java/control/grupoManager.java
+++ src/java/control/grupoManager.java
@@ -14,6 +14,7 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
 import model.Grupo;
+import org.apache.log4j.Logger;
 import org.primefaces.event.SelectEvent;
 import org.primefaces.event.UnselectEvent;
 
@@ -27,6 +28,9 @@ public class grupoManager implements Serializable {
 
     private Grupo grupo;
     private List<Grupo> grupos;
+    final static Logger logger = Logger.getLogger(GrupoManager.class);
+    private String acao = "";
+
 
     /**
      *
@@ -63,7 +67,8 @@ public class grupoManager implements Serializable {
     public List<Grupo> getGrupos() {
         // aqui entrarar a consulta ao Banco de dados para buscar os usuarios cadastrados
         grupos = new ArrayList<>();
-        return GrupoDao.getInstance().listaTodos();
+        grupos = GrupoDao.getInstance().listaTodos();
+        return grupos;
     }
 
     /**
@@ -94,5 +99,83 @@ public class grupoManager implements Serializable {
         FacesContext.getCurrentInstance().addMessage(null, msg);
     }
 
+     /**
+     *
+     * Remove a seleção atual
+     * @return Retorna a propria pagina
+     */
+    public String remover() {
+        if (grupo != null ) {
+            logger.info("grupo selecionado para ser removido " + grupo.getNome());
+            if (grupo.getNome().equals("admin")) {
+                logger.info("não pode remover admin");
+                addMessage("Atenção", "O Grupo admin não pode ser removido");
+            } else {
+                logger.info("chama o remover do dao de Grupo");
+                addMessage("Remover", GrupoDao.getInstance().remover(grupo));
+            }
     
+        }else{
+            addMessage("", "Favor Selecionar um Grupo");
+}
+        return "/restrito/grupo.xhtml";
+    }
+    
+    /**
+     *
+     * @return Pagina de Manter grupo
+     */
+    public String atualizar() {
+        if (grupo != null) {
+            if (grupo.getNome().equals("admin")) {
+                logger.info("Nao pode alterar o Admin");
+                addMessage("Atenção", "Grupo Admin não pode ser alterado");
+                return "/restrito/grupo.xhtml";
+            } else {
+                acao = "atualizar";
+                return "/restrito/grupoManter.xhtml";
+            }
+
+        } else {
+            addMessage("", "Favor selecionar um Grupo");
+            return "/restrito/grupo.xhtml";
+        }
+
+    }
+
+    /**
+     * Gerador de Mensagem pra tela
+     * @param summary
+     * @param detail
+     */
+    public void addMessage(String summary, String detail) {
+        FacesMessage message = new FacesMessage(summary, detail);
+        FacesContext.getCurrentInstance().addMessage(null, message);
+    }
+    
+        /**
+     * Grava o grupo ( Atualiza ou Inclui um Novo )
+     *
+     * @return Retorna para a pagina de Manutenção ou de lista de grupos.
+     */
+    public String gravar() {
+
+        System.out.println("Entrou no menu gravar");
+        logger.info("Chamou o Menu Gravar");
+        if (acao.equals("novo")) {
+            logger.info("acao novo encontrado");
+            addMessage("", GrupoDao.getInstance().salvar(grupo));
+            return "/restrito/grupo.xhtml";
+        } else {
+            if (acao.equals("atualizar")) {
+                logger.info("Acao atualizar encontrado");
+                logger.info("preencheu os requisitos... chama o DAO para Atualizar");
+                addMessage("", GrupoDao.getInstance().atualizar(grupo));
+                return "/restrito/grupo.xhtml";
+            } else {
+                return "/restrito/grupo.xhtml";
+            }
+        }
+    }
+         
 }
